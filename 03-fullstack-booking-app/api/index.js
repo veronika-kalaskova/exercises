8ce3f4679cec237e436e7e76bf604db4845ce3e5
veronika-kalaskova <veronika.kalaskova@gmail.com>
2024-10-14 17:44:54 +0200

    api: model, bcrypt

---
+++ 03-fullstack-booking-app/api/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const User = require("./models/user");
+require("dotenv").config();
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
+
+// prevents CORS error in network
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const userDoc = await User.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
+
+  res.json(userDoc);
+});
+
+app.listen(4000);
--- 03-fullstack-booking-app/client/src/pages/RegisterPage.jsx
+++ 03-fullstack-booking-app/client/src/pages/RegisterPage.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function registerUser(ev) {
+    ev.preventDefault()
+    axios.post('/register', {
+      name,
+      email,
+      password
+    })
+  }
+
   return (
     <div className="flex grow items-center justify-center">
       <div className="mb-64">
         <h1 className="mb-4 text-center text-4xl">Register</h1>
-        <form action="" className="mx-auto max-w-md">
-          <input type="text" placeholder="your name" />
-          <input type="email" placeholder="your email" />
-          <input type="password" placeholder="password" />
+        <form action="" className="mx-auto max-w-md" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="your name"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your email"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
           <button className="primary">Register</button>
           <div className="pt-2 text-center text-gray-500">
             Already registered?{" "}
